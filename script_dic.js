4a668aeb60d6382ff0d3264391020d37aabaf269
StefanCA89 <116792112+StefanCA89@users.noreply.github.com>
2022-11-09 10:46:27 +0200

    Add files via upload

---
+++ script_dic.js
@@ -0,0 +1,45 @@
+const dictionary = new Set()
+
+function areLetters(word) {
+    console.log(word)
+    let abc = /^[A-Za-z]+$/
+    if (word.match(abc)) {
+        return true
+    } else {
+        alert("Only letters please")
+        return false
+    }
+}
+
+function setWord() {
+    let word = document.getElementById("submitBox").value
+    document.getElementById("submitBox").value = ""
+    if (word != "" && areLetters(word)) {
+    dictionary.add(word)
+    }
+}
+
+function getWord() {
+    let word = document.getElementById("searchBox").value
+    document.getElementById("searchBox").value = ""
+    if (word == "" || !areLetters(word))
+    return
+    document.getElementById("searchBox").value = ""
+    if (dictionary.has(word)) {
+        document.getElementById("resultField").innerHTML = `\
+        <div class="card text-white bg-success">\
+            <div class="card-body">\
+                Word "${word}" is found. \
+            </div>\
+        </div>\
+        `
+    } else {
+        document.getElementById("resultField").innerHTML = `\
+        <div class="card w-25 text-white bg-danger">\
+            <div class="card-body">\
+                Word "${word}" is not found. \
+            </div>\
+        </div>\
+        `
+    }
+}
